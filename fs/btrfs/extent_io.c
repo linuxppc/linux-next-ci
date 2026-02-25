2037034d6dff1d4532bd9a2920097e1ec3168579
Bart Van Assche <bvanassche@acm.org>
2026-02-25 11:59:58 -0800

    btrfs: add missing RCU unlock in error path in try_release_subpage_extent_buffer()
    Call rcu_read_lock() before exiting the loop in
    try_release_subpage_extent_buffer() because there is a rcu_read_unlock()
    call past the loop.
    
    This has been detected by the Clang thread-safety analyzer.
    
    Fixes: ad580dfa388f ("btrfs: fix subpage deadlock in try_release_subpage_extent_buffer()")
    Reviewed-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: Boris Burkov <boris@bur.io>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -4486,6 +4486,7 @@ static int try_release_subpage_extent_buffer(struct folio *folio)
 		 */
 		if (!test_and_clear_bit(EXTENT_BUFFER_TREE_REF, &eb->bflags)) {
 			spin_unlock(&eb->refs_lock);
+			rcu_read_lock();
 			break;
 		}
 
